e943a7fa6666116a5f2ab01dd8a887332206dab5
meDracula <me.dracula@protonmail.com>
2024-10-06 12:40:36 +0200

    Alacritty flake and toml added

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+	description = "Alacritty configuration for meDracula as a plugin";
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+	};
+	outputs = { self, nixpkgs }:
+	let
+		system = "x86_64-linux";
+		pkgs = import nixpkgs { inherit system; };
+	in
+	{
+		devShells.${system} = {
+			neovim = pkgs.mkShell {
+				name = "alacritty";
+				packages = [
+					pkgs.alacritty
+				];
+				shellHook = ''echo "Alacrity" | figlet'';
+			};
+		};
+	};
+}
